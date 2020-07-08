b8a550092cc59e2cbd213a64994f58fdf8ea8921
Bruno Guerra <gguerrabruno@outlook.com>
2020-07-08 18:06:23 -0300

    Sample text on abut

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,11 @@ const IndexPage = () => (
     <Typography variant="h1" style={{textAlign: 'center'}}>
       Sobre
     </Typography>
+    <Typography variant="p" style={{textAlign: 'center'}}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus ut sapien eu iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec non lectus ac sem lobortis viverra at eu velit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam eget lobortis nisi. Quisque commodo a quam vel lobortis. In ultrices maximus lectus, sed blandit dolor pulvinar sed. Sed convallis orci eu libero laoreet, ut consectetur lacus volutpat. Pellentesque gravida pulvinar euismod. Phasellus nisi tortor, iaculis volutpat egestas a, aliquam id metus. Nullam quam diam, lobortis a lacinia ac, vestibulum a ex. Aenean velit urna, rhoncus et ullamcorper quis, scelerisque ut metus.
+
+    Etiam non ullamcorper ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ante orci, cursus a ullamcorper quis, interdum in enim. Donec efficitur urna ac tincidunt venenatis. Nulla quis gravida augue, at accumsan elit. Proin elit risus, dignissim et tincidunt at, convallis sed nunc. Etiam sed malesuada odio.
+    </Typography>
   </Layout>
 )
 
